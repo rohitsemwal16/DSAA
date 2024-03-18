86ddf607ad938af77f0be4763dd2a8f02c9158bf
Rohit Semwal <thisisrohit16@gmail.com>
2024-03-18 23:32:16 +0530

    Subsequence with sum K

---
+++ dsaa/src/main/java/org/rosemwal/code/array/ArraySubsequenceSumToK.java
@@ -0,0 +1,90 @@
+package org.rosemwal.code.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraySubsequenceSumToK {
+
+    static List<List<Integer>> subsequences = new ArrayList<>();
+
+    private static void getAllSubsequencesSumToK(int[] arr, ArrayList<Integer> output, int i, int sum, int k) {
+
+        if(i == arr.length) {
+            if(sum==k) {
+                subsequences.add(new ArrayList<>(output));
+            }
+            return;
+        }
+        output.add(arr[i]);
+        sum+=arr[i];
+        getAllSubsequencesSumToK(arr, output, i+1, sum, k);
+        sum-=arr[i];
+        output.remove(output.size()-1);
+        getAllSubsequencesSumToK(arr, output, i+1, sum, k);
+
+    }
+
+    public static void main(String[] args)  {
+
+        int[] arr = {1, 3, 2, 4, 5, 1};
+
+        getAllSubsequencesSumToK(arr,  new ArrayList<>(), 0, 0, 5);
+
+        for (int i=0; i<subsequences.size(); i++)    {
+            System.out.print(Arrays.toString(subsequences.get(i).toArray()));
+            System.out.println();
+        }
+
+        System.out.println("First subsequence.. ");
+        getFirstSubsequenceSumToK(arr, new ArrayList<>(), 0, 0, 5);
+
+        System.out.println("Number of Occurrences.. ");
+        System.out.println(getNumberOfOccurrences(arr, new ArrayList<>(), 0, 0, 5));
+
+    }
+
+    private static int getNumberOfOccurrences(int[] arr, ArrayList<Object> objects, int index, int sum, int k) {
+
+        if (index == arr.length)    {
+            if (sum == k)    {
+                return 1;
+            }   else {
+                return 0;
+            }
+        }
+
+        sum+=arr[index];
+        objects.add(arr[index]);
+        int left = getNumberOfOccurrences(arr, objects, index+1, sum, k);
+
+        sum-=arr[index];
+        objects.remove(objects.size()-1);
+        int right = getNumberOfOccurrences(arr, objects, index+1, sum, k);
+
+        return left+right;
+    }
+
+    private static boolean getFirstSubsequenceSumToK(int[] arr, ArrayList<Object> objects, int index, int sum, int k) {
+
+        if(index == arr.length)    {
+            if(sum == k) {
+                System.out.println(Arrays.toString(objects.toArray()));
+                return true;
+            }   else {
+                return false;
+            }
+        }
+
+        objects.add(arr[index]);
+        sum+=arr[index];
+        if (getFirstSubsequenceSumToK(arr, objects, index+1, sum, k)) return true;
+
+        sum-=arr[index];
+        objects.remove(objects.size()-1);
+        if (getFirstSubsequenceSumToK(arr, objects, index+1, sum, k)) return true;
+
+        return false;
+
+    }
+}
