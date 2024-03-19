67de69d6fd6c43643e45f9eabf34c77854d15a5e
Rohit Semwal <thisisrohit16@gmail.com>
2024-03-20 00:20:51 +0530

    Print all combinational sum

--- dsaa/src/main/java/org/rosemwal/code/array/ArraySubsequenceSumToK.java
+++ dsaa/src/main/java/org/rosemwal/code/array/ArraySubsequenceSumToK.java
@@ -2,6 +2,7 @@ package org.rosemwal.code.array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ArraySubsequenceSumToK {
@@ -41,6 +42,41 @@ public class ArraySubsequenceSumToK {
 
         System.out.println("Number of Occurrences.. ");
         System.out.println(getNumberOfOccurrences(arr, new ArrayList<>(), 0, 0, 5));
+
+
+        System.out.println("Multiple sum.. ");
+
+        int[] arr1 = {1,1, 2, 2,2,3,4,5,6};
+        subsequences = new LinkedList<>();
+        getAllSubsequencesSumToKOneElementMultipleTimes(arr1, new ArrayList<>(), 0, 0, 7);
+
+        for (int i=0; i<subsequences.size(); i++)    {
+            System.out.print(Arrays.toString(subsequences.get(i).toArray()));
+            System.out.println();
+        }
+
+    }
+
+    private static void getAllSubsequencesSumToKOneElementMultipleTimes(int[] arr, List<Integer> output, int index, int sum, int k)    {
+
+        if(index == arr.length) {
+            if(sum == k)    {
+                subsequences.add(new ArrayList<>(output));
+            }
+            return;
+        }
+
+        if(sum>k)   {
+            return;
+        }
+
+        sum+=arr[index];
+        output.add(arr[index]);
+        getAllSubsequencesSumToKOneElementMultipleTimes(arr, output, index, sum, k);
+
+        sum-=arr[index];
+        output.remove(output.size()-1);
+        getAllSubsequencesSumToKOneElementMultipleTimes(arr, output, index+1, sum, k);
 
     }
 
